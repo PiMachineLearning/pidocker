ffe3093569fbf1924fcf2892c86cb20bc6373e19
pandaninjas <admin@malwarefight.wip.la>
2024-12-21 13:39:36 -0800

    use more consistent download

--- main.sh
+++ main.sh
@@ -4,22 +4,13 @@ rm -f ./*.img
 rm -f ./*.xz
 sudo umount ./mount 
 rm -rf mount
-LATEST_PI_VER=$(curl 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/?C=M;O=A' 2>/dev/null | grep -oP '\d\d\d\d-\d\d-\d\d' | tail -n 1) # holy hell
 
-wget "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-$LATEST_PI_VER/$LATEST_PI_VER-raspios-bullseye-armhf-lite.img.xz"
+wget "https://downloads.raspberrypi.org/raspios_lite_armhf/root.tar.xz"
 
-xz -d "$LATEST_PI_VER-raspios-bullseye-armhf-lite.img.xz"
+xz -d root.tar.xz
 
-DEVFILE=$(sudo kpartx -av "$LATEST_PI_VER-raspios-bullseye-armhf-lite.img" | grep -oP "loop\dp2")
-
-mkdir mount
-
-sudo mount "/dev/mapper/$DEVFILE" ./mount
-
-sudo tar -czf - -C mount . > ./raspi.tar
-
-docker import - pimachinelearning/raspi-os-lite:"$LATEST_PI_VER" < raspi.tar
-docker import - pimachinelearning/raspi-os-lite:latest < raspi.tar
+docker import - pimachinelearning/raspi-os-lite:"$LATEST_PI_VER" < root.tar
+docker import - pimachinelearning/raspi-os-lite:latest < root.tar
 
 docker tag pimachinelearning/raspi-os-lite:"$LATEST_PI_VER" pimachinelearning/raspi-os-lite:"$LATEST_PI_VER"
 docker tag pimachinelearning/raspi-os-lite:latest pimachinelearning/raspi-os-lite:latest
